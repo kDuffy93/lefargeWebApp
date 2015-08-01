a6d5d2b918cf4f8e69b3358df561bbc4f2cb2aab
kDuffy93 <kyleduffy83@gmail.com>
2015-08-01 16:26:12 -0400

    working

--- Lefarge_FE_App/Models/drwdata.Designer.cs
+++ Lefarge_FE_App/Models/drwdata.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\kyle\OneDrive\Lefarge_FE_App\Lefarge_FE_App\Lefarge_FE_App\Models\drwdatabase.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\kyle\OneDrive\Lefarge_FE_App\Lefarge_FE_App\Lefarge_FE_App\Models\drwdata.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
